6cc63b1e79639422197ede56ef4506b9220e1eb9
amisadman <inboxsadman@gmail.com>
2025-10-20 13:11:39 +0600

    added dependancies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+ 
+
+  return (
+    <div className="flex justify-center items-center text-3xl text-black font-bold">
+      <h1 >Working...</h1>
+    </div>
+  )
+}
+
+export default App
